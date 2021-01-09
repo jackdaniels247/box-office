b0e65a7b5e23ec1a2ca4d8350ec606fcea3545dc
jackdaniels247 <64647294+jackdaniels247@users.noreply.github.com>
2021-01-10 02:46:40 +0530

    displayed show results and small fetch refracter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,25 @@
 import React,{useState} from 'react'
 import Mainpagelayout from '../components/Mainpagelayout'
+import {apiGet} from '../misc/config';
 
 const Home = () => {
      
     const [input, setInput]=useState(''); 
-
+    const[results,setResults]=useState(null);
 
     const onSearch=()=>{
 // http://api.tvmaze.com/search/shows?q=girls
+    
+    apiGet(`/search/shows?q=${input}`)
+    .then(result=> setResults(result));
 
-    fetch(`http://api.tvmaze.com/search/shows?q=${input}`).then(response=>response.json())
-    .then(result=>console.log(result));
 
-
+    
     }
 
     const onInputChange=(ev)=>{
         setInput(ev.target.value);
-        console.log(ev.target.value)
+        
     }
     
     const onKeyDown=(ev)=>{
@@ -26,11 +28,27 @@ const Home = () => {
         }
     }
 
+    const renderResults=()=>{
+        if(results && results.length===0){
+           return <div>No results</div>
+        }
+        if(results && results.length>0){
+           return <div>
+               {
+                   results.map(item=> <div key={item.show.id}>{item.show.name}</div>)
+               }
+           </div>
+        }
+        return null;
+    }
+
 
     return (
         <Mainpagelayout>
             <input type='text' onChange={onInputChange} value={input} onKeyDown={onKeyDown} />
             <input type='submit' value="Search" onClick={onSearch}  />
+
+            {renderResults()}
         </Mainpagelayout>
     )
 }
