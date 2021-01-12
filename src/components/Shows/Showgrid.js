280fc9810ffe7ef30cc083efb913c8d226a21040
jackdaniels247 <64647294+jackdaniels247@users.noreply.github.com>
2021-01-12 20:16:23 +0530

    using customhooks

--- src/components/Shows/Showgrid.js
+++ src/components/Shows/Showgrid.js
@@ -2,15 +2,51 @@ import React from 'react'
 import Showcard from './Showcard'
 import NOT_FOUND from '../../images/not-found.png';
 import { FlexGrid } from '../Styled';
+import { useShows } from '../../misc/CustomHooks';
 
-const Showgrid = ({data}) => 
-    (
+const Showgrid = ({data}) => {
+
+    const [starredState,dispatchStarred]=useShows();
+
+    
+
+
+    return(
         <FlexGrid>
         {
-            data.map(({show}) => <Showcard key={show.id} id={show.id} name={show.name} image={show.image?show.image.medium :NOT_FOUND} summary={show.summary}/>)
+            data.map(({show}) => {
+                const isStarred=starredState.includes(show.id); 
+
+                const onStarClick=()=>{
+                    if(isStarred){
+                        dispatchStarred({type:'REMOVE',showId:show.id});
+                    }
+                    else{
+                        dispatchStarred({type:'ADD',showId:show.id});
+                    }
+                }
+
+                return(
+                    <Showcard 
+                    key={show.id} 
+                    id={show.id} 
+                    name={show.name} 
+                    image={show.image?show.image.medium :NOT_FOUND} 
+                    summary={show.summary}
+                    onStarClick={onStarClick}
+                    isStarred={isStarred}
+                    />)
+            }
+               
+                    )
+            
+            
+            
         }
         </FlexGrid>
     )
+}
+    
 
 
 export default Showgrid
--- src/misc/CustomHooks.js
+++ src/misc/CustomHooks.js
@@ -19,14 +19,14 @@ function showsReducer(prevState,action){
 
 
 function usePersistedReducer(reducer,initialState,key){
-    const [state,dispatch]=useReducer(reducer,initialState,()=>{
+    const [state,dispatch]=useReducer(reducer,initialState,(initial)=>{
     const persisted=localStorage.getItem(key);
 
     return persisted ? JSON.parse(persisted):initial;
     })
 
     useEffect(()=>{
-        localStorage.setItem(key,JSON.stringify(this.state.))
+        localStorage.setItem(key,JSON.stringify(state))
     },[state,key]);
 
     return [state,dispatch];
